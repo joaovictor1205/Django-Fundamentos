3c1e477e2fcaf0a34071eebb1477db8031c97568
joaovictor1205 <joaovictor.lol12@gmail.com>
2018-06-27 08:59:06 -0300

    CRUD Completo

---
+++ contas/views.py
@@ -0,0 +1,54 @@
+from django.shortcuts import render, redirect
+import datetime
+from .models import Transacao
+from .forms import TransacaoForm
+
+# Create your views here.
+
+def home(request):
+
+    data = {}
+    data['transacoes'] = ['t1', 't2', 't3']
+
+    data['now'] = datetime.datetime.now()
+
+    return render(request, 'contas/home.html')
+
+def listagem(request):
+
+    data = {}
+    data['transacoes'] = Transacao.objects.all()
+
+    return render(request, 'contas/listagem.html', data)
+
+def nova_transacao(request):
+
+    data = {}
+    form = TransacaoForm(request.POST or None)
+
+    if form.is_valid():
+        form.save()
+        return redirect('url_listagem')
+
+    data['form'] = form
+
+    return render(request, 'contas/form.html', data)
+
+def update(request, pk):
+    data = {}
+    transacao = Transacao.objects.get(pk=pk)
+    form = TransacaoForm(request.POST or None, instance=transacao)
+
+    if form.is_valid():
+        form.save()
+        return redirect('url_listagem')
+
+    data['form'] = form
+    data['transacao'] = transacao
+
+    return render(request, 'contas/form.html', data)
+
+def delete(request, pk):
+    transacao = Transacao.objects.get(pk=pk)
+    transacao.delete()
+    return redirect('url_listagem')
